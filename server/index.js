b6fe9f47893f75326d2192b4acd49b9fa659989d
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-05-25 20:48:50 -0400

    added workout controller

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const express = require('express')
 // Initialize the app object.
 const app = express()
 
+app.use('/places', require('./controllers/workouts'))
+
 // Create a homepage route.
 app.get('/', function (req, res) {
-  // This gets sent to the client
-  // (your web browser most likely!)
   res.send('Hello world')
 })
 
