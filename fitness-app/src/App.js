4f2e5b4c21ddf6bac49418a0e759945212aeb2a6
lcobell <lcobell@gmail.com>
2022-06-03 14:14:22 -0500

    unstyled login page

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  // Login react states
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  //Mock login info
+  const loginData = [
+    {
+      username: "fit-appUser1",
+      password: "getfit1",
+    },
+    // can add more users as objects here...
+  ];
+  const invalid = {
+    uname: "not a valid username",
+    pword: "not a valid password",
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+  // submit form function, prevent reload
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    var { uname, pword } = document.forms[0];
+
+    //Find login info
+    const userData = loginData.find((user) => user.username === uname.value);
+
+    //comparison
+    if (userData) {
+      if (userData.password !== pword.value) {
+        // incorrect password
+        setErrorMessages({ name: "pword", message: invalid.pword });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      //user not found
+      setErrorMessages({ name: "uname", message: invalid.uname });
+    }
+  };
+
+  //JSX for Login
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username</label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+          <label>Password</label>
+          <input type="text" name="pword" required />
+          {renderErrorMessage("pword")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
     </div>
   );
 }
