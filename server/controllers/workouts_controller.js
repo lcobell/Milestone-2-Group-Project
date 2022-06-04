0b872ea367951f39800650770efd69f1225531e2
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-06-04 14:49:16 -0400

    deleted all renders except for index page

--- server/controllers/workouts_controller.js
+++ server/controllers/workouts_controller.js
@@ -4,25 +4,11 @@ const db = require('../models')
 //HOME
 router.get('/', (req, res) => {
   db.Workout.find()
-    .then((workouts) => {
-      res.render('index', { workouts })
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.render('index')
-    })
 })
 
 //CREATE
 router.post('/', (req, res) => {
   db.Workout.create(req.body)
-    .then(() => {
-      res.redirect('index')
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.render('index')
-    })
 })
 
 //NEW
@@ -33,48 +19,21 @@ router.get('/new', (req, res) => {
 //DETAILS
 router.get('/:id', (req, res) => {
   db.Workout.findById(req.params.id)
-    .then((workout) => {
-      res.render('index', { workout })
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.render('index')
-    })
 })
 
 //UPDATE
 router.put('/:id', (req, res) => {
   db.Workout.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => {
-      res.redirect(index)
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.render('index')
-    })
 })
 
 //DELETE
 router.delete('/:id', (req, res) => {
   db.Workout.findByIdAndDelete(req.params.id)
-    .then((workout) => {
-      res.redirect('index')
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.render('index')
-    })
 })
 
 //EDIT
 router.get('/:id/edit', (req, res) => {
   db.Workout.findById(req.params.id)
-    .then((workout) => {
-      res.render('index', { workout })
-    })
-    .catch((err) => {
-      res.render('index')
-    })
 })
 
 module.exports = router
