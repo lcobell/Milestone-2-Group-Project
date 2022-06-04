0b5bdbebda58c4b92facb4824661d343bb7ad32e
josiahw7240 <71360953+josiahw7240@users.noreply.github.com>
2022-06-04 14:35:31 -0400

    Navbar and styling

--- fitness-app/src/components/Navbar.js
+++ fitness-app/src/components/Navbar.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button.Js";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleclick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            Gainz Tracker <i className="fab fa-typo3"></i>
+            Gainz Tracker <i class="fa-brands fa-wolf-pack-battalion"></i>
           </Link>
           <div className="menu-icon" onClick={handleclick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
