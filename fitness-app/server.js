9a486780f1b4ee94585fc18725b7e60f03cd5966
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-06-11 13:22:34 -0400

    heroku

--- fitness-app/server.js
+++ fitness-app/server.js
@@ -1,19 +1,23 @@
-const express = require("express");
+const express = require('express')
 const mongoose = require('mongoose')
-const cors = require("cors")
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 //connect to mongoose
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-  )
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('connected to mongo: ', process.env.MONGO_URI)
+  }
+)
 //require route
-app.use("/", require("./routes/workoutRoute"))
+app.use('/', require('./routes/workoutRoute'))
 
-app.listen(3001, function(){
-    console.log("express server is running on port 3001")
+app.listen(process.env.PORT, function () {
+  console.log('express server is running on port 3001')
 })
