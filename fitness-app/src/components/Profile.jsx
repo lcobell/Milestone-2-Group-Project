c739b10a755b4e1250b4950b81389b23f8f4f1bb
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-06-11 12:37:44 -0400

    added forms succesfully

--- fitness-app/src/components/Profile.jsx
+++ fitness-app/src/components/Profile.jsx
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "./Navbar";
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
 
 function Profile() {
   const [workouts, setWorkouts] = useState([
     {
-      title: "",
-      content: "",
+      title: '',
+      content: '',
+      sets: '',
+      lbs: '',
+      reps: '',
+      timer: '',
     },
-  ]);
+  ])
 
   useEffect(() => {
-    fetch("/profile")
+    fetch('/profile')
       .then((res) => {
         if (res.ok) {
-          return res.json();
+          return res.json()
         }
       })
-      .then((jsonRes) => setWorkouts(jsonRes));
-  });
+      .then((jsonRes) => setWorkouts(jsonRes))
+  })
 
   return (
     <div className="container">
-        <Navbar />
+      <Navbar />
       <h1>Routines Page</h1>
       {workouts.map((workout) => (
         <div>
           <h1>{workout.title}</h1>
           <p>{workout.content}</p>
+          <p>{workout.sets}</p>
+          <p>{workout.lbs}</p>
+          <p>{workout.reps}</p>
+          <p>{workout.timer}</p>
         </div>
       ))}
     </div>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
--- fitness-app/src/components/Routine.jsx
+++ fitness-app/src/components/Routine.jsx
@@ -1,52 +1,108 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Navbar from "./Navbar";
+import React, { useState } from 'react'
+import axios from 'axios'
+import Navbar from './Navbar'
 
 function Routine() {
-    const [input, setInput] = useState({
-        title: '',
-        content: ''
+  const [input, setInput] = useState({
+    title: '',
+    content: '',
+    sets: '',
+    lbs: '',
+    reps: '',
+    timer: '',
+  })
+
+  function handleChange(e) {
+    const { name, value } = e.target
+
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      }
     })
+  }
 
-    function handleChange(e) {
-        const {name, value} = e.target;
-
-        setInput(prevInput =>{
-            return{
-                ...prevInput,
-                [name]: value
-            }
-        })
+  function handleClick(e) {
+    e.preventDefault()
+    const newRoutine = {
+      title: input.title,
+      content: input.content,
+      sets: input.sets,
+      lbs: input.lbs,
+      reps: input.reps,
+      timer: input.timer,
     }
+    axios.post('http://localhost:3001/routine', newRoutine)
+  }
 
-    function handleClick(e) {
-        e.preventDefault();
-        const newRoutine = {
-            title: input.title,
-            content: input.content
-        }
-        axios.post('http://localhost:3001/routine', newRoutine)
-    }
+  return (
+    <div className="container">
+      <Navbar />
+      <h1>Create Routine</h1>
+      <form>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            name="title"
+            value={input.title}
+            className="form-control"
+          ></input>
+        </div>
 
-    return <div className="container">
-        <Navbar />
-        <h1>
-            Create Routine
-        </h1>
-        <form>
-            <div className='form-group'>
-            <input onChange={handleChange} name="title" value={input.title} className='form-control'></input>
-            </div>
+        <div className="form-group">
+          <textarea
+            onChange={handleChange}
+            name="content"
+            value={input.content}
+            className="form-control"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="sets">Sets</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            name="sets"
+            value={input.sets}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lbs">Weight</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            name="lbs"
+            value={input.lbs}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reps">Reps</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            name="reps"
+            value={input.reps}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="timer">Time</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            name="timer"
+            value={input.timer}
+            className="form-control"
+          />
+        </div>
 
-            <div className='form-group'>
-            <textarea onChange={handleChange} name="content" value={input.content} className='form-control'></textarea>
-            </div>
-
-            <button onClick={handleClick} >ADD ROUTINE</button>
-        </form>
-
-
+        <button onClick={handleClick}>ADD ROUTINE</button>
+      </form>
     </div>
+  )
 }
 
-export default Routine;
+export default Routine
